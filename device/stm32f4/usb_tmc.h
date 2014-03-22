2ec3aef518f55829d1b2291a20a2ce70c28384af
Karl Palsson <karlp@tweak.net.au>
2014-03-22 19:32:11 +0000

    usb: tidy up whitespace and DECLS and headernames
    Some leftovers from copying a file and then gutting it.

--- device/stm32f4/usb_tmc.h
+++ device/stm32f4/usb_tmc.h
@@ -20,34 +20,31 @@
  * XXXXX
  */
 
-#ifndef USB_CDCACM_H
-#define	USB_CDCACM_H
+#ifndef USB_TMC_H
+#define	USB_TMC_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
+#include <libopencm3/cm3/common.h>
 #include <libopencm3/usb/usbd.h>
-	
-	struct  __attribute__ ((packed))  usb_tmc_get_capabilities_response {
-		uint8_t USBTMC_status;
-		uint8_t reserved0;
-		uint16_t bcdUSBTMC;
-		uint8_t interface_capabilities;  /* bitmap! */
-		uint8_t device_capabilities;  /* bitmap! */
-		uint8_t reserved1[6];
-		uint8_t reserved_subclass[12];
-		
-	};
 
-	void usb_tmc_init(usbd_device **usb_dev, const char *serial_number);
-	void usb_tmc_setup_pre_arch(void);
-	void usb_tmc_setup_post_arch(void);
-	void tmc_glue_send_data(uint8_t *buf, size_t len);
+struct __attribute__((packed)) usb_tmc_get_capabilities_response
+{
+	uint8_t USBTMC_status;
+	uint8_t reserved0;
+	uint16_t bcdUSBTMC;
+	uint8_t interface_capabilities; /* bitmap! */
+	uint8_t device_capabilities; /* bitmap! */
+	uint8_t reserved1[6];
+	uint8_t reserved_subclass[12];
+};
 
-#ifdef	__cplusplus
-}
-#endif
+BEGIN_DECLS
 
-#endif	/* USB_CDCACM_H */
+void usb_tmc_init(usbd_device **usb_dev, const char *serial_number);
+void usb_tmc_setup_pre_arch(void);
+void usb_tmc_setup_post_arch(void);
+void tmc_glue_send_data(uint8_t *buf, size_t len);
+
+END_DECLS
+
+#endif	/* USB_TMC_H */
 
